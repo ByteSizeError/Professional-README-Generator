332ba86d64b047213d4308084157c8dffe72a91e
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-04 12:49:43 -0700

    writing to file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Professional-README-Generator
+#hi
+## Desctiption
+ hisdf
+## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+
+  - [Credits](#credits)
+
+  - [License](#license)
+## Usage
+ sdf
+## Installation
+ shdfsdf
+## How to Contribute
+ asdf
+## Tests
+ sdf
+## Questions
+ https://github.com/ByteSizeError
+ You can also reach me at jimmyz0622@gmail.com for additional questions.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -34,10 +35,10 @@ const questions = [
     name: 'tests',
   },
   {
-    type: 'checkbox',
+    type: 'list',
     message: 'Licenses:',
     name: 'license',
-    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    choices: ['Apache License 2.0', 'Boost Software License 1.0', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'ISC License', 'MIT License', 'Mozilla Public License 2.0', 'The Unlicense'],
   },
   {
     type: 'input',
@@ -52,7 +53,41 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, `# ${data.title}\n`, (err) =>
+    err ? console.log(err) : console.log('Title Success!')
+  );
+  fs.appendFile(fileName, `## Description\n ${data.description}\n`, (err) =>
+    err ? console.log(err) : console.log('Description Success!')
+  );
+
+  fs.appendFile(fileName, `## Table of Contents\n
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)\n`
+    , (err) =>
+      err ? console.log(err) : console.log('Table of Contents Success!')
+  );
+  fs.appendFile(fileName, `## Installation\n ${data.installation}\n`, (err) =>
+    err ? console.log(err) : console.log('Installation Success!')
+  );
+  fs.appendFile(fileName, `## Usage\n ${data.usage}\n`, (err) =>
+    err ? console.log(err) : console.log('Usage Success!')
+  );
+
+  // add license
+
+  fs.appendFile(fileName, `## How to Contribute\n ${data.contributing}\n`, (err) =>
+    err ? console.log(err) : console.log('Contributing Success!')
+  );
+  fs.appendFile(fileName, `## Tests\n ${data.tests}\n`, (err) =>
+    err ? console.log(err) : console.log('Tests Success!')
+  );
+  fs.appendFile(fileName, `## Questions\n https://github.com/${data.username}\n You can also reach me at ${data.email} for additional questions.`, (err) =>
+    err ? console.log(err) : console.log('Questions Success!')
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -60,11 +95,11 @@ function init() {
     .prompt(
       questions
     )
-    .then((response) =>
-      response.confirm === response.password
-        ? console.log('Success!')
-        : console.log('Failure in creating README')
-    );
+    .then((data) => {
+      const fileName = 'README.md';
+      writeToFile(fileName, data);
+
+    });
 }
 
 // Function call to initialize app
