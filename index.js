98b9a6c1fb9893fcd07623dfe7f4c538eb6fe2c2
Jimmy Zheng <jimmyz0622@gmail.com>
2021-10-04 12:02:13 -0700

    added questions

--- index.js
+++ index.js
@@ -2,36 +2,69 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Project Title:',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Description:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Installation Instructions:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Usage Information:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Contribution Guidelines:',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Test Instructions:',
+    name: 'tests',
+  },
+  {
+    type: 'checkbox',
+    message: 'Licenses:',
+    name: 'license',
+    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+  },
+  {
+    type: 'input',
+    message: 'GitHub Username:',
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: 'Email:',
+    name: 'email',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+  inquirer
+    .prompt(
+      questions
+    )
+    .then((response) =>
+      response.confirm === response.password
+        ? console.log('Success!')
+        : console.log('Failure in creating README')
+    );
 }
 
 // Function call to initialize app
